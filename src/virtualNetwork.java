f91f0dfb34a570a625cb9df665402ba3327b7343
osiepna <jferidrico@gmail.com>
2014-01-29 15:39:16 +0300

    virtual Mobile

--- src/virtualNetwork.java
+++ src/virtualNetwork.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class virtualNetwork {
 
@@ -6,11 +8,32 @@ public class virtualNetwork {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PhoneNumber phone = new PhoneNumber(111);
+		System.out.println("Welcome to the Virtual Mobile Network");
+		System.out.println("Enter your Phone number");
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		PhoneNumber phone = new PhoneNumber(number);
+		System.out.println("You have the following new messages");
 		phone.notifications();
-		phone.sendSms("Heloo", 123);
-		phone.viewInbox();
-		phone.viewOutbox();
+		System.out.println("Choose 1 to send an sms, 2 to view inbox and 3 to view outbox");
+		int choice = scan.nextInt();
+		if(choice == 1){
+			System.out.println("Enter the number to send to");
+			number = scan.nextInt();
+			System.out.println("Enter the message");
+			scan.nextLine();
+			String msg = scan.nextLine();
+			phone.sendSms(msg, number);
+		}
+		else if (choice == 2){
+			phone.viewInbox();
+		}
+		else if(choice == 3){
+			phone.viewOutbox();
+		}
+		else{
+			System.out.println("Wrong Choice");
+		}
 	}
 
 }
